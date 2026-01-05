b596db18127216ed48fca4d09f10abfa662065c7
Siraddeen <85670787+Siraddeen@users.noreply.github.com>
2026-01-05 09:32:46 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30000,
-  });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully.');
     } catch (error) {
         console.log(error);
