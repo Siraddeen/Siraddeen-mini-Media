c3e4768fa97d31f69ee861543a2c70840acb4410
Siraddeen <85670787+Siraddeen@users.noreply.github.com>
2026-01-05 12:01:12 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,26 +1,26 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log('mongodb connected successfully.');
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// export default connectDB;
-
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30000,
-  });
-  console.log("MongoDB connected successfully");
-};
-
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+}
 export default connectDB;
+
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   await mongoose.connect(process.env.MONGO_URI, {
+//     serverSelectionTimeoutMS: 30000,
+//   });
+//   console.log("MongoDB connected successfully");
+// };
+
+// export default connectDB;
 
 
 
