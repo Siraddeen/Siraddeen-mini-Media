15a4bce8722e0d22f99f07912662261ee6ba8813
Siraddeen <85670787+Siraddeen@users.noreply.github.com>
2026-01-05 09:08:18 +0530

    Add server selection timeout to MongoDB connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,21 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 30000,
+  });
         console.log('mongodb connected successfully.');
     } catch (error) {
         console.log(error);
     }
 }
 export default connectDB;
+
+
+
+// const connectDB = async () => {
+//   await mongoose.connect(process.env.MONGO_URI, {
+//     serverSelectionTimeoutMS: 30000,
+//   });
+//   console.log("MongoDB connected");
+// };
